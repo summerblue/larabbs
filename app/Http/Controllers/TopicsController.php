382511a6951618241de60d2789876305581cd6c0
Summer <summer@yousails.com>
2017-10-31 23:30:24 +0000

    Topic order

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -14,11 +14,11 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
-	{
-		$topics = Topic::with('user', 'category')->paginate(30);
+	public function index(Request $request, Topic $topic)
+    {
+        $topics = $topic->withOrder($request->order)->paginate(20);
         return view('topics.index', compact('topics'));
-	}
+    }
 
     public function show(Topic $topic)
     {
--- resources/views/topics/index.blade.php
+++ resources/views/topics/index.blade.php
@@ -17,8 +17,8 @@
 
             <div class="panel-heading">
                 <ul class="nav nav-pills">
-                    <li role="presentation" class="active"><a href="#">最后回复</a></li>
-                    <li role="presentation"><a href="#">最新发布</a></li>
+                    <li class="{{ active_class(( ! if_query('order', 'recent') )) }}"><a href="{{ Request::url() }}?order=default">最后回复</a></li>
+                    <li class="{{ active_class(if_query('order', 'recent')) }}"><a href="{{ Request::url() }}?order=recent">最新发布</a></li>
                 </ul>
             </div>
 
