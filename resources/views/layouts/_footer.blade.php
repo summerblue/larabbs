d223f5f97006c0db43caa9375c635179d1fdd539
Summer <summer@yousails.com>
2017-11-01 08:36:52 +0000

    Admin can manage site settings

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
             由 <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">Summer</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
-        <p class="pull-right"><a href="mailto:name@email.com">联系我们</a></p>
+        <p class="pull-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
     </div>
 </footer>
