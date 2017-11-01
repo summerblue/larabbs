81a600e07c6a59e0ba7e5e01b8ce83580225f213
Summer <summer@yousails.com>
2017-11-01 09:08:45 +0000

    Setup default home page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 
 // Authentication Routes...
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
