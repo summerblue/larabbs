e80be90628b0a147cac47d1c26b0b9a61d047a00
Summer <summer@yousails.com>
2017-11-01 09:09:56 +0000

    User default avatar after signup

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -9,13 +9,11 @@ use App\Models\User;
 
 class UserObserver
 {
-    public function creating(User $user)
+    public function saving(User $user)
     {
-        //
-    }
-
-    public function updating(User $user)
-    {
-        //
+        // 这样写扩展性更高，只有空的时候才指定默认头像
+        if (empty($user->avatar)) {
+            $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/30/1/TrJS40Ey5k.png';
+        }
     }
 }
