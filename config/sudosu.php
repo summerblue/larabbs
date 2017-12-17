aecdf83f630d91329117c905be6e0d27bf34ceb4
Yu Li <liyu001989@gmail.com>
2017-12-17 21:25:30 +0800

    add test to allowed_tlds

--- config/sudosu.php
+++ config/sudosu.php
@@ -3,7 +3,7 @@
 return [
 
     // 允许使用的顶级域名
-    'allowed_tlds' => ['dev', 'local', 'app'],
+    'allowed_tlds' => ['dev', 'local', 'app', 'test'],
 
     // 用户模型
     'user_model' => App\Models\User::class
