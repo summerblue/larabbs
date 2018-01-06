56c9a2d5b865e9a29132fc8765d0b4013f2cf40e
Yu Li <liyu001989@gmail.com>
2018-01-06 22:58:39 +0800

    add easysms

---
+++ app/Providers/EasySmsServiceProvicer.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Overtrue\EasySms\EasySms;
+use Illuminate\Support\ServiceProvider;
+
+class EasySmsServiceProvicer extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(EasySms::class, function ($app) {
+            return new EasySms(config('easysms'));
+        });
+
+        $this->app->alias(EasySms::class, 'easysms');
+    }
+}
---
+++ database/migrations/2018_01_02_224500_add_phone_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPhoneToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('phone')->nullable()->index()->after('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('phone');
+        });
+    }
+}
