5988b3e60d2c12219af50b9db906665e622dd3d5
Yu Li <liyu001989@gmail.com>
2018-01-15 23:01:24 +0800

    add jwt

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Controllers\Api;
 
+use Auth;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\Api\AuthorizationRequest;
 use App\Http\Requests\Api\SocialAuthorizationRequest;
 
 class AuthorizationsController extends Controller
 {
+    public function store(AuthorizationRequest $request)
+    {
+        $username = $request->username;
+
+        filter_var($username, FILTER_VALIDATE_EMAIL) ?
+            $credentials['email'] = $username :
+            $credentials['phone'] = $username;
+
+        $credentials['password'] = $request->password;
+
+        if (!$token = \Auth::guard('api')->attempt($credentials)) {
+            return $this->response->errorUnauthorized('用户名或密码错误');
+        }
+
+        return $this->respondWithToken($token);
+    }
+
     public function socialStore($type, SocialAuthorizationRequest $request)
     {
         if (!in_array($type, ['weixin'])) {
@@ -30,26 +49,48 @@ class AuthorizationsController extends Controller
 
             $oauthUser = $driver->userFromToken($token);
         } catch (\Exception $e) {
-            return $this->response->error('参数错误，未获取用户信息', 422);
+            return $this->response->errorUnauthorized('参数错误，未获取用户信息');
         }
 
         switch ($type) {
-            case 'weixin':
-                $user = User::where('weixin_unionid', $oauthUser->offsetGet('unionid'))->first();
+        case 'weixin':
+            $user = User::where('weixin_unionid', $oauthUser->offsetGet('unionid'))->first();
 
-                // 没有用户，默认创建一个用户
-                if (!$user) {
-                    $user = User::create([
-                        'name' => $oauthUser->getNickname(),
-                        'avatar' => $oauthUser->getAvatar(),
-                        'weixin_openid' => $oauthUser->getId(),
-                        'weixin_unionid' => $oauthUser->offsetGet('unionid'),
-                    ]);
-                }
+            // 没有用户，默认创建一个用户
+            if (!$user) {
+                $user = User::create([
+                    'name' => $oauthUser->getNickname(),
+                    'avatar' => $oauthUser->getAvatar(),
+                    'weixin_openid' => $oauthUser->getId(),
+                    'weixin_unionid' => $oauthUser->offsetGet('unionid'),
+                ]);
+            }
 
-                break;
+            break;
         }
 
-        return $this->response->array(['token' => $user->id]);
+        $token = Auth::guard('api')->fromUser($user);
+        return $this->respondWithToken($token);
+    }
+
+    public function update()
+    {
+        $token = Auth::guard('api')->refresh();
+        return $this->respondWithToken($token);
+    }
+
+    public function destroy()
+    {
+        Auth::logout();
+        return $this->response->noContent();
+    }
+
+    protected function respondWithToken($token)
+    {
+        return $this->response->array([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'expires_in' => Auth::guard('api')->factory()->getTTL() * 60
+        ]);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Auth;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Traits\LastActivedAtHelper;
     use Traits\ActiveUserHelper;
@@ -88,5 +89,25 @@ class User extends Authenticatable
         }
 
         $this->attributes['avatar'] = $path;
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
