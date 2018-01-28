14092afe11efb9ce1943376aecde38128815a4cc
Yu Li <liyu001989@gmail.com>
2018-01-28 19:32:33 +0800

    api request fix

--- app/Http/Requests/Api/TopicRequest.php
+++ app/Http/Requests/Api/TopicRequest.php
@@ -2,20 +2,8 @@
 
 namespace App\Http\Requests\Api;
 
-use Dingo\Api\Http\FormRequest;
-
 class TopicRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
     /**
      * Get the validation rules that apply to the request.
      *
