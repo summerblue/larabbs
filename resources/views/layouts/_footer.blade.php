023a230606bc2f0fc1612be0f0cc6f81ff14d41e
luosilent <luosilent@qq.com>
2018-05-11 06:48:37 +0000

    add easywechat

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="container">
         <p class="pull-left">
-            由 <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">Summer</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+            by <a href="https://github.com/luosilent" target="_blank">luosilent Github</a>  <span style="color: #e27575;font-size: 14px;">stars❤</span>
         </p>
 
         <p class="pull-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
