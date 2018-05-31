aaf105c7392a7ee5d2d2bc14bc402e7b13afb522
Yu Li <liyu001989@gmail.com>
2018-05-31 18:09:16 +0800

    update phone regex

--- app/Http/Requests/Api/CaptchaRequest.php
+++ app/Http/Requests/Api/CaptchaRequest.php
@@ -12,7 +12,11 @@ class CaptchaRequest extends FormRequest
     public function rules()
     {
         return [
-            'phone' => 'required|regex:/^1[34578]\d{9}$/|unique:users',
+            'phone' => [
+                'required',
+                'regex:/^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\d{8}$/',
+                'unique:users'
+            ]
         ];
     }
 }
