d3f1b981f4c0182536ea20bbd93af9d54bdff5b6
vagrant <vagrant@homestead>
2018-07-08 11:34:12 +0000

    add easysms

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        App\Providers\EasySmsServiceProvider::class,
     ],
 
     /*
---
+++ config/easysms.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    // HTTP 请求的超时时间（秒）
+    'timeout' => 5.0,
+
+    // 默认发送配置
+    'default' => [
+        // 网关调用策略，默认：顺序调用
+        'strategy' => \Overtrue\EasySms\Strategies\OrderStrategy::class,
+
+        // 默认可用的发送网关
+        'gateways' => [
+            'yunpian',
+        ],
+    ],
+    // 可用的网关配置
+    'gateways' => [
+        'errorlog' => [
+            'file' => '/tmp/easy-sms.log',
+        ],
+        'yunpian' => [
+            'api_key' => env('YUNPIAN_API_KEY'),
+        ],
+    ],
+];
