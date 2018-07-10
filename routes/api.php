ed8e37c5b9f361ea3ea532f6a539b62a3afe77a8
vagrant <vagrant@homestead>
2018-07-10 14:34:04 +0000

    rate limit

--- routes/api.php
+++ routes/api.php
@@ -34,10 +34,17 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api'
 ], function($api) {
-    // 短信验证码
-    $api->post('verificationCodes', 'VerificationCodesController@store')
-        ->name('api.verificationCodes.store');
-    //用户注册
-    $api->post('users','UsersController@store')
-        ->name('api.users.store');
+    $api->group([
+        'middleware'=> 'api.throttle',
+        'limit' => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires'),
+    ], function($api) {
+        // 短信验证码
+        $api->post('verificationCodes', 'VerificationCodesController@store')
+            ->name('api.verificationCodes.store');
+        //用户注册
+        $api->post('users','UsersController@store')
+            ->name('api.users.store');
+    });
+
 });
