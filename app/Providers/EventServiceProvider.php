dfa462e8cefbe3eeb89e099eb0001bb962ce9397
vagrant <vagrant@homestead>
2018-07-11 13:42:12 +0000

    add socialiteproviders

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -13,8 +13,11 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\Event' => [
-            'App\Listeners\EventListener',
+//        'App\Events\Event' => [
+//            'App\Listeners\EventListener',
+//        ],
+        \SocialiteProviders\Manager\SocialiteWasCalled::class => [
+            'SocialiteProviders\Weixin\WeixinExtendSocialite@handle'
         ],
     ];
 
