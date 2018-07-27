eb427e62f867b8259740a3c439eb8793ec5a6c2b
liyu <liyu001989@gmail.com>
2018-07-27 07:00:45 +0000

    add rap2hpoutre/laravel-log-viewer

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,5 @@ Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
 Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
 Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
+
+Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
