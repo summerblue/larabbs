76427013f4be315862ee3c0bb8c74373c28077df
liyu <liyu001989@gmail.com>
2018-08-24 08:51:41 +0000

    beyondcode/laravel-self-diagnosis

---
+++ app/Checks/JwtSecretIsSet.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Checks;
+
+use BeyondCode\SelfDiagnosis\Checks\Check;
+
+class JwtSecretIsSet implements Check
+{
+    /**
+     * The name of the check.
+     *
+     * @param array $config
+     * @return string
+     */
+    public function name(array $config): string
+    {
+        return 'Jwt Secret is set';
+    }
+
+    /**
+     * Perform the actual verification of this check.
+     *
+     * @param array $config
+     * @return bool
+     */
+    public function check(array $config): bool
+    {
+        return config('jwt.secret') != null;
+    }
+
+    /**
+     * The error message to display in case the check does not pass.
+     *
+     * @param array $config
+     * @return string
+     */
+    public function message(array $config): string
+    {
+        return 'JWT_SECRET 没有被设置';
+    }
+}
