efd5e8fa95841c8f6cb2f2cd19a39523843dfa9c
Summer <summer@learnku.com>
2018-12-26 08:44:25 +0000

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
