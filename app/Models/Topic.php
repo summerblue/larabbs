4e7cd46cdc8ce14c829fdb2afef976d4915174a9
Summer <summer@learnku.com>
2018-12-26 09:05:48 +0000

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,5 +4,18 @@ namespace App\Models;
 
 class Topic extends Model
 {
-    protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+    protected $fillable = [
+        'title', 'body', 'user_id', 'category_id', 'reply_count',
+        'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
