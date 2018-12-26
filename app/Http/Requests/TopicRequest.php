08b79bc76d77384008d92f5a210226f6d1c8a8ec
Summer <summer@learnku.com>
2018-12-26 12:10:41 +0000

    用户可以创建话题

--- app/Http/Requests/TopicRequest.php
+++ app/Http/Requests/TopicRequest.php
@@ -10,17 +10,14 @@ class TopicRequest extends Request
         {
             // CREATE
             case 'POST':
-            {
-                return [
-                    // CREATE ROLES
-                ];
-            }
             // UPDATE
             case 'PUT':
             case 'PATCH':
             {
                 return [
-                    // UPDATE ROLES
+                    'title'       => 'required|min:2',
+                    'body'        => 'required|min:3',
+                    'category_id' => 'required|numeric',
                 ];
             }
             case 'GET':
@@ -35,7 +32,8 @@ class TopicRequest extends Request
     public function messages()
     {
         return [
-            // Validation messages
+            'title.min' => '标题必须至少两个字符',
+            'body.min' => '文章内容必须至少三个字符',
         ];
     }
 }
--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,8 +5,7 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = [
-        'title', 'body', 'user_id', 'category_id', 'reply_count',
-        'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug',
+        'title', 'body', 'category_id', 'excerpt', 'slug'
     ];
 
     public function category()
