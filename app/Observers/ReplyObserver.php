a911536d86f8701a81ab4c0bb911880bf9a5c0f3
Summer <summer@learnku.com>
2018-12-26 12:49:15 +0000

    删除评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -7,13 +7,13 @@ use App\Models\Reply;
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
+use App\Notifications\TopicReplied;
+
 class ReplyObserver
 {
     public function created(Reply $reply)
     {
-        $reply->topic->reply_count = $reply->topic->replies->count();
-        $reply->topic->save();
-
+        $reply->topic->updateReplyCount();
         // 通知话题作者有新的评论
         $reply->topic->user->notify(new TopicReplied($reply));
     }
@@ -21,5 +21,10 @@ class ReplyObserver
     public function creating(Reply $reply)
     {
         $reply->content = clean($reply->content, 'user_topic_body');
+    }
+
+    public function deleted(Reply $reply)
+    {
+        $reply->topic->updateReplyCount();
     }
 }
