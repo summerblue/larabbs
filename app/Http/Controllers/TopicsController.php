e4ed897d0a675579a355112e55b7bfc6b7069728
Summer <summer@learnku.com>
2018-12-26 09:01:49 +0000

    生成话题骨架

---
+++ app/Http/Controllers/TopicsController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Topic;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\TopicRequest;
+
+class TopicsController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+
+	public function index()
+	{
+		$topics = Topic::paginate();
+		return view('topics.index', compact('topics'));
+	}
+
+    public function show(Topic $topic)
+    {
+        return view('topics.show', compact('topic'));
+    }
+
+	public function create(Topic $topic)
+	{
+		return view('topics.create_and_edit', compact('topic'));
+	}
+
+	public function store(TopicRequest $request)
+	{
+		$topic = Topic::create($request->all());
+		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+	}
+
+	public function edit(Topic $topic)
+	{
+        $this->authorize('update', $topic);
+		return view('topics.create_and_edit', compact('topic'));
+	}
+
+	public function update(TopicRequest $request, Topic $topic)
+	{
+		$this->authorize('update', $topic);
+		$topic->update($request->all());
+
+		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+	}
+
+	public function destroy(Topic $topic)
+	{
+		$this->authorize('destroy', $topic);
+		$topic->delete();
+
+		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+	}
+}
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,5 @@ Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('ver
 Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
