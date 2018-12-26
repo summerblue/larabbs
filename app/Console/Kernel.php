5c65c550a41feb2e69f7ea322364a06b4e9c592d
Summer <summer@learnku.com>
2018-12-26 13:37:28 +0000

    用户最后活跃时间

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,9 +26,10 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-
-        // 一小时执行一次『活跃用户』数据生成的命令
+        // 每隔一个小时执行一遍
         $schedule->command('larabbs:calculate-active-user')->hourly();
+        // 每日零时执行一次
+        $schedule->command('larabbs:sync-user-actived-at')->dailyAt('00:00');
     }
 
     /**
--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,7 @@ use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
+    use Traits\LastActivedAtHelper;
     use Traits\ActiveUserHelper;
     use HasRoles;
     use MustVerifyEmailTrait;
