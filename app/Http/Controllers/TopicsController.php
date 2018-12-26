4a42479d2c23d672be049942c7b587a2f2f9b30e
Summer <summer@learnku.com>
2018-12-26 09:06:59 +0000

    修复 N+1 问题

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -16,7 +16,7 @@ class TopicsController extends Controller
 
 	public function index()
 	{
-		$topics = Topic::paginate();
+		$topics = Topic::with('user', 'category')->paginate(30);
 		return view('topics.index', compact('topics'));
 	}
 
