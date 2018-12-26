217820aac0a19f4646c06b7c7905cae0166ad930
Summer <summer@learnku.com>
2018-12-26 12:23:19 +0000

    用户可以删除自己发的话题

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -58,13 +58,13 @@ class TopicsController extends Controller
         return redirect()->route('topics.show', $topic->id)->with('success', '更新成功！');
     }
 
-	public function destroy(Topic $topic)
-	{
-		$this->authorize('destroy', $topic);
-		$topic->delete();
+    public function destroy(Topic $topic)
+    {
+        $this->authorize('destroy', $topic);
+        $topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
-	}
+        return redirect()->route('topics.index')->with('success', '成功删除！');
+    }
 
     public function uploadImage(Request $request, ImageUploadHandler $uploader)
     {
--- app/Models/User.php
+++ app/Models/User.php
@@ -23,4 +23,9 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         return $this->hasMany(Topic::class);
     }
+
+    public function isAuthorOf($model)
+    {
+        return $this->id == $model->user_id;
+    }
 }
