abd1e14e7c4f202e2bace0d28299769e2e40cd17
liyu <liyu001989@gmail.com>
2019-01-15 10:49:29 +0800

    fix avatar

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -13,7 +13,7 @@ class UserObserver
     {
         // 这样写扩展性更高，只有空的时候才指定默认头像
         if (empty($user->avatar)) {
-            $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/30/1/TrJS40Ey5k.png';
+            $user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/30/1/TrJS40Ey5k.png';
         }
     }
 }
