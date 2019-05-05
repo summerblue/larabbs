e054d45c9b368192faeacfcd27f03290c73263ae
vagrant <vagrant@homestead>
2019-05-05 06:04:59 +0000

    小测试

--- resources/views/topics/index.blade.php
+++ resources/views/topics/index.blade.php
@@ -17,7 +17,7 @@
 
             <div class="panel-heading">
                 <ul class="nav nav-pills">
-                    <li class="{{ active_class(( ! if_query('order', 'recent') )) }}"><a href="{{ Request::url() }}?order=default">最后回复</a></li>
+                    <li class="{{ active_class(( ! if_query('order', 'recent') )) }}"><a href="{{ Request::url() }}?order=default">最后1回复</a></li>
                     <li class="{{ active_class(if_query('order', 'recent')) }}"><a href="{{ Request::url() }}?order=recent">最新发布</a></li>
                 </ul>
             </div>
