aca49580618d4f64d8ae94adb3ecb2de1d6d08a6
Summer <summer@learnku.com>
2019-05-07 01:09:32 +0000

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -4,3 +4,8 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+function category_nav_active($category_id)
+{
+    return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
+}
--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,5 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
