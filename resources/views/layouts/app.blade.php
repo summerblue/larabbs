19cea82e1092b47ee5c57255192dfdfd15e34bd7
Summer <summer@learnku.com>
2019-05-07 02:39:06 +0000

    站点权限部署

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,6 +35,10 @@
     @include('layouts._footer')
   </div>
 
+  @if (app()->isLocal())
+    @include('sudosu::user-selector')
+  @endif
+
   <!-- Scripts -->
   <script src="{{ mix('js/app.js') }}"></script>
 
