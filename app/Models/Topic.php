6592c8ec33efaebf88a4f1824666e0e9a99d2064
Summer <summer@learnku.com>
2019-05-07 00:56:46 +0000

    生成话题骨架

---
+++ app/Models/Topic.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Models;
+
+class Topic extends Model
+{
+    protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+}
