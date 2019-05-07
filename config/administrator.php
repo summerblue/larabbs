75b6e77bed7375c9d0b5b12876ce1e1be55a63cc
Summer <summer@learnku.com>
2019-05-07 03:32:08 +0000

    内容管理后台

--- config/administrator.php
+++ config/administrator.php
@@ -42,6 +42,11 @@ return [
             'roles',
             'permissions',
         ],
+        '内容管理' => [
+            'categories',
+            'topics',
+            'replies',
+        ],
     ],
 
     /*
