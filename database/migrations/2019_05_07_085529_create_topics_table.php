80bfdad55ed8547f99b34ee85fe62809f8b9e822
Summer <summer@learnku.com>
2019-05-07 04:19:00 +0000

    联动删除

--- database/migrations/2019_05_07_085529_create_topics_table.php
+++ database/migrations/2019_05_07_085529_create_topics_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTopicsTable extends Migration 
+class CreateTopicsTable extends Migration
 {
 	public function up()
 	{
@@ -11,7 +11,7 @@ class CreateTopicsTable extends Migration
             $table->increments('id');
             $table->string('title')->index();
             $table->text('body');
-            $table->integer('user_id')->unsigned()->index();
+            $table->bigInteger('user_id')->unsigned()->index();
             $table->integer('category_id')->unsigned()->index();
             $table->integer('reply_count')->unsigned()->default(0);
             $table->integer('view_count')->unsigned()->default(0);
--- database/migrations/2019_05_07_095559_create_replies_table.php
+++ database/migrations/2019_05_07_095559_create_replies_table.php
@@ -3,14 +3,14 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRepliesTable extends Migration 
+class CreateRepliesTable extends Migration
 {
 	public function up()
 	{
 		Schema::create('replies', function(Blueprint $table) {
             $table->increments('id');
             $table->integer('topic_id')->unsigned()->default(0)->index();
-            $table->integer('user_id')->unsigned()->default(0)->index();
+            $table->bigInteger('user_id')->unsigned()->default(0)->index();
             $table->text('content');
             $table->timestamps();
         });
