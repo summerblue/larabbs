15cd7dab9cf4c545d4dbf8cf564db064fbb82821
Summer <summer@learnku.com>
2019-05-07 01:11:03 +0000

    话题排序

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,10 +8,13 @@ use App\Models\Category;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category)
+    public function show(Category $category, Request $request, Topic $topic)
     {
         // 读取分类 ID 关联的话题，并按每 20 条分页
-        $topics = Topic::where('category_id', $category->id)->paginate(20);
+        $topics = $topic->withOrder($request->order)
+                        ->where('category_id', $category->id)
+                        ->paginate(20);
+
         // 传参变量话题和分类到模板中
         return view('topics.index', compact('topics', 'category'));
     }
