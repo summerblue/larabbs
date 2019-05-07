3fa37360bd078151ed8a5ff449d9a38a933caca1
Summer <summer@learnku.com>
2019-05-07 02:10:52 +0000

    消息通知列表

---
+++ app/Http/Controllers/NotificationsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class NotificationsController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        // 获取登录用户的所有通知
+        $notifications = Auth::user()->notifications()->paginate(20);
+        // 标记为已读，未读数量清零
+        Auth::user()->markAsRead();
+        return view('notifications.index', compact('notifications'));
+    }
+}
