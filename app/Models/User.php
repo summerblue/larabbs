9c1fd68f3510f60a98db103d625db563a108bcf1
LiYu <liyu001989@gmail.com>
2019-05-09 11:11:53 +0800

    user register using phone

--- app/Models/User.php
+++ app/Models/User.php
@@ -35,7 +35,7 @@ class User extends Authenticatable implements MustVerifyEmailContract
     }
 
     protected $fillable = [
-        'name', 'email', 'password', 'introduction', 'avatar',
+        'name', 'phone', 'email', 'password', 'introduction', 'avatar',
     ];
 
     protected $hidden = [
