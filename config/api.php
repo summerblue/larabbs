77faeb25ea58fe6a8b3b6e0aa5b7b5e68e2b91f1
LiYu <liyu001989@gmail.com>
2019-05-09 11:30:20 +0800

    rate limit

--- config/api.php
+++ config/api.php
@@ -229,4 +229,19 @@ return [
 
     ],
 
+    /*
+     * 接口频率限制
+     */
+    'rate_limits' => [
+        // 访问频率限制，次数/分钟
+        'access' => [
+            'expires' => env('RATE_LIMITS_EXPIRES', 1),
+            'limit'  => env('RATE_LIMITS', 60),
+        ],
+        // 登录相关，次数/分钟
+        'sign' => [
+            'expires' => env('SIGN_RATE_LIMITS_EXPIRES', 1),
+            'limit'  => env('SIGN_RATE_LIMITS', 10),
+        ],
+    ],
 ];
