aefbddf21ca990b8373e456327d220fa2d75f121
LiYu <liyu001989@gmail.com>
2019-05-10 13:20:23 +0800

    generate-token command

---
+++ app/Console/Commands/GenerateToken.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class GenerateToken extends Command
+{
+    protected $signature = 'larabbs:generate-token';
+
+    protected $description = '快速为用户生成 token';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $userId = $this->ask('输入用户 id');
+
+        $user = User::find($userId);
+
+        if (!$user) {
+            return $this->error('用户不存在');
+        }
+
+        // 一年以后过期
+        $ttl = 365*24*60;
+        $this->info(\Auth::guard('api')->setTTL($ttl)->fromUser($user));
+    }
+}
