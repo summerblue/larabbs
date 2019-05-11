ec14a1ebddf89b8ce77296485cad319b9340689c
LiYu <liyu001989@gmail.com>
2019-05-11 22:39:20 +0800

    categories index

--- routes/api.php
+++ routes/api.php
@@ -55,6 +55,8 @@ $api->version('v1', [
         'expires' => config('api.rate_limits.access.expires'),
     ], function ($api) {
         // 游客可以访问的接口
+        $api->get('categories', 'CategoriesController@index')
+            ->name('api.categories.index');
 
         // 需要 token 验证的接口
         $api->group(['middleware' => 'api.auth'], function($api) {
