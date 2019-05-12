01bac439635f6cfdb68e1c85166725dd13a2bbb3
LiYu <liyu001989@gmail.com>
2019-05-12 12:39:05 +0800

    notifications read

--- app/Http/Controllers/Api/NotificationsController.php
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -20,4 +20,11 @@ class NotificationsController extends Controller
 			'unread_count' => $this->user()->notification_count,
 		]);
 	}
+
+    public function read()
+	{
+		$this->user()->markAsRead();
+
+		return $this->response->noContent();
+	}
 }
--- routes/api.php
+++ routes/api.php
@@ -100,6 +100,9 @@ $api->version('v1', [
             // 通知统计
             $api->get('user/notifications/stats', 'NotificationsController@stats')
                 ->name('api.user.notifications.stats');
+            // 标记消息通知为已读
+            $api->patch('user/read/notifications', 'NotificationsController@read')
+                ->name('api.user.notifications.read');
         });
     });
 });
