20d139e2a5198fb2d165f299a63ccbb3234f8bd2
LiYu <liyu001989@gmail.com>
2019-05-12 12:48:29 +0800

    permissions index

---
+++ app/Transformers/PermissionTransformer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Transformers;
+
+use Spatie\Permission\Models\Permission;
+use League\Fractal\TransformerAbstract;
+
+class PermissionTransformer extends TransformerAbstract
+{
+    public function transform(Permission $permission)
+    {
+        return [
+            'id' => $permission->id,
+            'name' => $permission->name,
+        ];
+    }
+}
