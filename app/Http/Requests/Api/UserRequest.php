27a3ac3b4d83461d7431dfbcd3c9afe1bb67ab47
LiYu <liyu001989@gmail.com>
2019-08-08 09:42:57 +0800

    fix user request email

--- app/Http/Requests/Api/UserRequest.php
+++ app/Http/Requests/Api/UserRequest.php
@@ -20,7 +20,7 @@ class UserRequest extends FormRequest
 
             return [
                 'name' => 'between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' .$userId,
-                'email' => 'email',
+                'email'=>'email|unique:users,email,'.$userId,
                 'introduction' => 'max:80',
                 'avatar_image_id' => 'exists:images,id,type,avatar,user_id,'.$userId,
             ];
