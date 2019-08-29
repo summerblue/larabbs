2bfd146303707f2db19cd73d64e57fe7b710d666
Summer <summer@yousails.com>
2019-08-29 19:14:05 +0800

    fix default avatar link

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -10,7 +10,7 @@ class UserObserver
     {
         // 这样写扩展性更高，只有空的时候才指定默认头像
         if (empty($user->avatar)) {
-            $user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/30/1/TrJS40Ey5k.png';
+            $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/30/1/TrJS40Ey5k.png';
         }
     }
 }
