92184842b5569143fba537c470df6d5b6a942661
Summer <summer@learnku.com>
2019-09-19 11:03:15 +0000

    显示个人资料

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable implements MustVerifyEmailContract
     use Notifiable, MustVerifyEmailTrait;
 
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'introduction',
     ];
 
     protected $hidden = [
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -10,11 +10,11 @@
     <div class="card ">
       <img class="card-img-top" src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/600/h/600" alt="{{ $user->name }}">
       <div class="card-body">
-            <h5><strong>个人简介</strong></h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-            <hr>
-            <h5><strong>注册于</strong></h5>
-            <p>January 01 1901</p>
+        <h5><strong>个人简介</strong></h5>
+        <p>{{ $user->introduction }}</p>
+        <hr>
+        <h5><strong>注册于</strong></h5>
+        <p>{{ $user->created_at->diffForHumans() }}</p>
       </div>
     </div>
   </div>
