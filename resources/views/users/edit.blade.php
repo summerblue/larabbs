c84cc3ae9c658082121ec9442b68b2036b73003a
Summer <summer@learnku.com>
2019-09-19 11:01:16 +0000

    编辑个人资料

---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+  <div class="col-md-8 offset-md-2">
+
+    <div class="card">
+      <div class="card-header">
+        <h4>
+          <i class="glyphicon glyphicon-edit"></i> 编辑个人资料
+        </h4>
+      </div>
+
+      <div class="card-body">
+
+        <form action="{{ route('users.update', $user->id) }}" method="POST" accept-charset="UTF-8">
+          <input type="hidden" name="_method" value="PUT">
+          <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+          @include('shared._error')
+
+          <div class="form-group">
+            <label for="name-field">用户名</label>
+            <input class="form-control" type="text" name="name" id="name-field" value="{{ old('name', $user->name) }}" />
+          </div>
+          <div class="form-group">
+            <label for="email-field">邮 箱</label>
+            <input class="form-control" type="text" name="email" id="email-field" value="{{ old('email', $user->email) }}" />
+          </div>
+          <div class="form-group">
+            <label for="introduction-field">个人简介</label>
+            <textarea name="introduction" id="introduction-field" class="form-control" rows="3">{{ old('introduction', $user->introduction) }}</textarea>
+          </div>
+          <div class="well well-sm">
+            <button type="submit" class="btn btn-primary">保存</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+@endsection
