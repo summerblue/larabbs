0cff7c6b4495d025de13d3c68df4e78f30ec3bb5
Summer <summer@learnku.com>
2019-09-19 10:15:04 +0000

    邮箱认证

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,5 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 
 // Email 认证相关路由
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
-Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
-Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
+Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
