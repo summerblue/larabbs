bcdbd083ef9d92affec1b4f694958e872ee6ccf6
Summer <summer@learnku.com>
2019-09-19 09:36:58 +0000

    移动 User 模型到 app/models 目录

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+
 
 class User extends Authenticatable
 {
