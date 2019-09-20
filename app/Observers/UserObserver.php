71be4659b6a57f0c6d2f8c16706740e437880492
Summer <summer@learnku.com>
2019-09-20 04:35:38 +0000

    注册用户默认头像

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -4,18 +4,13 @@ namespace App\Observers;
 
 use App\Models\User;
 
-// creating, created, updating, updated, saving,
-// saved,  deleting, deleted, restoring, restored
-
 class UserObserver
 {
-    public function creating(User $user)
+    public function saving(User $user)
     {
-        //
-    }
-
-    public function updating(User $user)
-    {
-        //
+        // 这样写扩展性更高，只有空的时候才指定默认头像
+        if (empty($user->avatar)) {
+            $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/30/1/TrJS40Ey5k.png';
+        }
     }
 }
