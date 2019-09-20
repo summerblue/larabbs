21255132f326f67b5029b27e47f128b1929611cb
Summer <summer@learnku.com>
2019-09-20 02:23:56 +0000

    集成 laravel-permission 扩展包

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,13 @@ use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
-
 use Auth;
+use Spatie\Permission\Traits\HasRoles;
+
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
+    use HasRoles;
     use MustVerifyEmailTrait;
 
     use Notifiable {
---
+++ database/migrations/2019_09_20_102258_seed_roles_and_permissions_data.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class SeedRolesAndPermissionsData extends Migration
+{
+    public function up()
+    {
+        // 需清除缓存，否则会报错
+        app(Spatie\Permission\PermissionRegistrar::class)->forgetCachedPermissions();
+
+        // 先创建权限
+        Permission::create(['name' => 'manage_contents']);
+        Permission::create(['name' => 'manage_users']);
+        Permission::create(['name' => 'edit_settings']);
+
+        // 创建站长角色，并赋予权限
+        $founder = Role::create(['name' => 'Founder']);
+        $founder->givePermissionTo('manage_contents');
+        $founder->givePermissionTo('manage_users');
+        $founder->givePermissionTo('edit_settings');
+
+        // 创建管理员角色，并赋予权限
+        $maintainer = Role::create(['name' => 'Maintainer']);
+        $maintainer->givePermissionTo('manage_contents');
+    }
+
+    public function down()
+    {
+        // 需清除缓存，否则会报错
+        app(Spatie\Permission\PermissionRegistrar::class)->forgetCachedPermissions();
+
+        // 清空所有数据表数据
+        $tableNames = config('permission.table_names');
+
+        Model::unguard();
+        DB::table($tableNames['role_has_permissions'])->delete();
+        DB::table($tableNames['model_has_roles'])->delete();
+        DB::table($tableNames['model_has_permissions'])->delete();
+        DB::table($tableNames['roles'])->delete();
+        DB::table($tableNames['permissions'])->delete();
+        Model::reguard();
+    }
+}
