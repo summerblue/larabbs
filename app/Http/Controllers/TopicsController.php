d7f9c7024b1a20314dee69b9a31682e5898035c7
Summer <summer@learnku.com>
2019-09-20 01:07:09 +0000

    翻译 URL 标示

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -23,8 +23,13 @@ class TopicsController extends Controller
         return view('topics.index', compact('topics'));
     }
 
-    public function show(Topic $topic)
+    public function show(Request $request, Topic $topic)
     {
+        // URL 矫正
+        if ( ! empty($topic->slug) && $topic->slug != $request->slug) {
+            return redirect($topic->link(), 301);
+        }
+
         return view('topics.show', compact('topic'));
     }
 
@@ -40,7 +45,7 @@ class TopicsController extends Controller
         $topic->user_id = Auth::id();
         $topic->save();
 
-        return redirect()->route('topics.show', $topic->id)->with('success', '帖子创建成功！');
+        return redirect()->to($topic->link())->with('success', '成功创建话题！');
     }
 
     public function edit(Topic $topic)
@@ -55,7 +60,7 @@ class TopicsController extends Controller
         $this->authorize('update', $topic);
         $topic->update($request->all());
 
-        return redirect()->route('topics.show', $topic->id)->with('success', '更新成功！');
+        return redirect()->to($topic->link())->with('success', '更新成功！');
     }
 
     public function destroy(Topic $topic)
