e2207b2907a072de9da545dd23ed4f655fab235c
liyu <liyu001989@gmail.com>
2019-09-24 16:59:00 +0800

    API 基础配置

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,5 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->name('api.v1.')->group(function() {
 });
