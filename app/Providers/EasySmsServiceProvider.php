4fc5f08951da04307b5f4d56ab8a10373f8cc85b
liyu <liyu001989@gmail.com>
2019-09-25 22:48:30 +0800

    短信调试

---
+++ app/Providers/EasySmsServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Overtrue\EasySms\EasySms;
+use Illuminate\Support\ServiceProvider;
+
+class EasySmsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(EasySms::class, function ($app) {
+            return new EasySms(config('easysms'));
+        });
+
+        $this->app->alias(EasySms::class, 'easysms');
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
