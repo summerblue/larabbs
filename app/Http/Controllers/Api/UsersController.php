dfc54be54a53758f3f58d1eef6fcfead108a453e
liyu <liyu001989@gmail.com>
2019-10-05 16:43:35 +0800

    用户注册

---
+++ app/Http/Controllers/Api/UsersController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Resources\UserResource;
+use App\Http\Requests\Api\UserRequest;
+use Illuminate\Auth\AuthenticationException;
+
+class UsersController extends Controller
+{
+    public function store(UserRequest $request)
+    {
+        $verifyData = \Cache::get($request->verification_key);
+
+        if (!$verifyData) {
+            abort(403, '验证码已失效');
+        }
+
+        if (!hash_equals($verifyData['code'], $request->verification_code)) {
+            // 返回401
+            throw new AuthenticationException('验证码错误');
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'phone' => $verifyData['phone'],
+            'password' => $request->password,
+        ]);
+
+        // 清除验证码缓存
+        \Cache::forget($request->verification_key);
+
+        return new UserResource($user);
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\Resources\Json\Resource;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -29,5 +30,7 @@ class AppServiceProvider extends ServiceProvider
 		\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
 		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
         \App\Models\Link::observe(\App\Observers\LinkObserver::class);
+
+        Resource::withoutWrapping();
     }
 }
