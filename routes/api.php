d7ced335e7174d4d7be76daf9a7035e8191758cf
liyu <liyu001989@gmail.com>
2019-10-05 17:33:01 +0800

    频率限制

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,10 @@ use Illuminate\Http\Request;
 |
  */
 
-Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function () {
+Route::prefix('v1')->namespace('Api')
+                   ->name('api.v1.')
+                   ->middleware('throttle:'.config('api.rate_limits.sign'))
+                   ->group(function () {
     // 短信验证码
     Route::post('verificationCodes', 'VerificationCodesController@store')
         ->name('verificationCodes.store');
