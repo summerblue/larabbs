20f5d417d70dbd10d423ba2a8af340fb33091bd2
liyu <liyu001989@gmail.com>
2019-10-10 11:03:37 +0800

    删除话题

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -26,4 +26,13 @@ class TopicsController extends Controller
 
         return new TopicResource($topic);
     }
+
+    public function destroy(Topic $topic)
+    {
+        $this->authorize('destroy', $topic);
+
+        $topic->delete();
+
+        return response(null, 204);
+    }
 }
