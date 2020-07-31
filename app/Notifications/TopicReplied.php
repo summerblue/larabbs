0392b32cc65269c14ccea786b783e8062f030c63
Summer <summer@learnku.com>
2020-07-31 11:22:44 +0000

    消息通知有新回复

---
+++ app/Notifications/TopicReplied.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use App\Models\Reply;
+
+class TopicReplied extends Notification
+{
+    use Queueable;
+
+    public $reply;
+
+    public function __construct(Reply $reply)
+    {
+        // 注入回复实体，方便 toDatabase 方法中的使用
+        $this->reply = $reply;
+    }
+
+    public function via($notifiable)
+    {
+        // 开启通知的频道
+        return ['database'];
+    }
+
+    public function toDatabase($notifiable)
+    {
+        $topic = $this->reply->topic;
+        $link =  $topic->link(['#reply' . $this->reply->id]);
+
+        // 存入数据库里的数据
+        return [
+            'reply_id' => $this->reply->id,
+            'reply_content' => $this->reply->content,
+            'user_id' => $this->reply->user->id,
+            'user_name' => $this->reply->user->name,
+            'user_avatar' => $this->reply->user->avatar,
+            'topic_link' => $link,
+            'topic_id' => $topic->id,
+            'topic_title' => $topic->title,
+        ];
+    }
+}
--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -7,12 +7,17 @@ use App\Models\Reply;
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
+use App\Notifications\TopicReplied;
+
 class ReplyObserver
 {
     public function created(Reply $reply)
     {
         $reply->topic->reply_count = $reply->topic->replies->count();
         $reply->topic->save();
+
+        // 通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicReplied($reply));
     }
 
     public function creating(Reply $reply)
