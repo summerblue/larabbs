82c4a0a7af0e628993db61add8cc51dfc0da8654
Summer <summer@learnku.com>
2020-07-31 08:53:37 +0000

    分类话题列表

--- resources/views/topics/index.blade.php
+++ resources/views/topics/index.blade.php
@@ -1,11 +1,17 @@
 @extends('layouts.app')
 
-@section('title', '话题列表')
+@section('title', isset($category) ? $category->name : '话题列表')
 
 @section('content')
 
 <div class="row mb-5">
   <div class="col-lg-9 col-md-9 topic-list">
+    @if (isset($category))
+      <div class="alert alert-info" role="alert">
+        {{ $category->name }} ：{{ $category->description }}
+      </div>
+    @endif
+
     <div class="card ">
 
       <div class="card-header bg-transparent">
