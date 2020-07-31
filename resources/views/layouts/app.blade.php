eecab14203f9984e0d6bcaf73629c7abbd476a73
Summer <summer@learnku.com>
2020-07-31 09:40:29 +0000

    WYSIWYG 编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,8 @@
   <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
+  @yield('styles')
+
 </head>
 
 <body>
@@ -34,6 +36,8 @@
 
   <!-- Scripts -->
   <script src="{{ mix('js/app.js') }}"></script>
+
+  @yield('scripts')
 </body>
 
 </html>
