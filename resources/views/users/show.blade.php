deb96680b96c4733e7ec8f0d34611b122f436ffd
Summer <summer@learnku.com>
2020-07-31 08:56:46 +0000

    用户话题列表

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -27,9 +27,13 @@
     <hr>
 
     {{-- 用户发布的内容 --}}
-    <div class="card ">
+    <div class="card">
       <div class="card-body">
-        暂无数据 ~_~
+        <ul class="nav nav-tabs">
+          <li class="nav-item"><a class="nav-link active bg-transparent" href="#">Ta 的话题</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Ta 的回复</a></li>
+        </ul>
+        @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
       </div>
     </div>
 
