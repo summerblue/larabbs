cfde6a752e554520306a633e11d1517596df3be6
liyu <liyu001989@gmail.com>
2020-08-02 18:01:47 +0800

    短信调试

---
+++ config/easysms.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    // HTTP 请求的超时时间（秒）
+    'timeout' => 10.0,
+
+    // 默认发送配置
+    'default' => [
+        // 网关调用策略，默认：顺序调用
+        'strategy' => \Overtrue\EasySms\Strategies\OrderStrategy::class,
+
+        // 默认可用的发送网关
+        'gateways' => [
+            'aliyun',
+        ],
+    ],
+    // 可用的网关配置
+    'gateways' => [
+        'errorlog' => [
+            'file' => '/tmp/easy-sms.log',
+        ],
+        'aliyun' => [
+            'access_key_id' => env('SMS_ALIYUN_ACCESS_KEY_ID'),
+            'access_key_secret' => env('SMS_ALIYUN_ACCESS_KEY_SECRET'),
+            'sign_name' => 'Larabbs',
+        ],
+    ],
+];
