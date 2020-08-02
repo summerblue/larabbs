3f8aa777950ae015099c012481fb6c1d637d0066
liyu <liyu001989@gmail.com>
2020-08-02 17:33:27 +0800

    API 基础配置

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,6 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::prefix('v1')->name('api.v1.')->group(function() {
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
 });
