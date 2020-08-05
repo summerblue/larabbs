d3a563be362f84c3948380bdd5c919cddc831fd1
liyu <liyu001989@gmail.com>
2020-08-05 22:34:59 +0800

    add socialite

--- config/services.php
+++ config/services.php
@@ -35,4 +35,10 @@ return [
         'key'   => env('BAIDU_TRANSLATE_KEY'),
     ],
 
+    'wechat' => [
+        'client_id' => env('WEIXIN_KEY'),
+        'client_secret' => env('WEIXIN_SECRET'),
+        'redirect' => env('WEIXIN_REDIRECT_URI'),
+    ],
+
 ];
