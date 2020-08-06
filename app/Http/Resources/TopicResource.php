066749811f0e4450b25a8c4b26d2a9a8be481160
liyu <liyu001989@gmail.com>
2020-08-07 00:18:41 +0800

    话题列表

--- app/Http/Resources/TopicResource.php
+++ app/Http/Resources/TopicResource.php
@@ -6,14 +6,12 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class TopicResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        $data = parent::toArray($request);
+        $data['user'] = new UserResource($this->whenLoaded('user'));
+        $data['category'] = new CategoryResource($this->whenLoaded('category'));
+
+        return $data;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -57,6 +57,9 @@ Route::prefix('v1')
                 // 分类列表
                 Route::get('categories', 'CategoriesController@index')
                     ->name('categories.index');
+                // 某个用户发布的话题
+                Route::get('users/{user}/topics', 'TopicsController@userIndex')
+                    ->name('users.topics.index');
                 // 话题列表，详情
                 Route::resource('topics', 'TopicsController')->only([
                     'index', 'show'
