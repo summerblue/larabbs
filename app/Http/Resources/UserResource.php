31f6413da4f182e8e6965c35ef89b758be2d087c
liyu <liyu001989@gmail.com>
2020-08-06 23:42:33 +0800

    用户信息

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -6,14 +6,26 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
+    protected $showSensitiveFields = false;
+
     public function toArray($request)
     {
-        return parent::toArray($request);
+        if (!$this->showSensitiveFields) {
+            $this->resource->makeHidden(['phone', 'email']);
+        }
+
+        $data = parent::toArray($request);
+
+        $data['bound_phone'] = $this->resource->phone ? true : false;
+        $data['bound_wechat'] = ($this->resource->weixin_unionid || $this->resource->weixin_openid) ? true : false;
+
+        return $data;
+    }
+
+    public function showSensitiveFields()
+    {
+        $this->showSensitiveFields = true;
+
+        return $this;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -49,6 +49,17 @@ Route::prefix('v1')
 
         Route::middleware('throttle:' . config('api.rate_limits.access'))
             ->group(function () {
+                // 游客可以访问的接口
 
+                // 某个用户的详情
+                Route::get('users/{user}', 'UsersController@show')
+                    ->name('users.show');
+
+                // 登录后可以访问的接口
+                Route::middleware('auth:api')->group(function() {
+                    // 当前登录用户信息
+                    Route::get('user', 'UsersController@me')
+                        ->name('user.show');
+                });
 			});
     });
