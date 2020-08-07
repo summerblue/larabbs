13c28a734b16a2239899dfc8cf2a6cdbc357b67a
liyu <liyu001989@gmail.com>
2020-08-07 22:40:20 +0800

    显示用户角色

---
+++ app/Http/Resources/RoleResource.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RoleResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+        ];
+    }
+}
