c23d74a8bcd6d9d8760b9adc7e59d7aa8c23e75f
liyu <liyu001989@gmail.com>
2020-08-07 22:45:20 +0800

    活跃用户

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -62,4 +62,10 @@ class UsersController extends Controller
 
         return (new UserResource($user))->showSensitiveFields();
     }
+
+    public function activedIndex(User $user)
+    {
+        UserResource::wrap('data');
+        return UserResource::collection($user->getActiveUsers());
+    }
 }
