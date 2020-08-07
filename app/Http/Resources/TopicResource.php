6358f4c1a2f7505b1454d0ded3ba10a3e680aecf
liyu <liyu001989@gmail.com>
2020-08-07 23:27:44 +0800

    话题单元测试

--- app/Http/Resources/TopicResource.php
+++ app/Http/Resources/TopicResource.php
@@ -8,10 +8,22 @@ class TopicResource extends JsonResource
 {
     public function toArray($request)
     {
-        $data = parent::toArray($request);
-        $data['user'] = new UserResource($this->whenLoaded('user'));
-        $data['category'] = new CategoryResource($this->whenLoaded('category'));
-
-        return $data;
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'body' => $this->body,
+            'category_id' => (int)$this->category_id,
+            'user_id' => (int)$this->user_id,
+            'reply_count' => (int)$this->reply_count,
+            'view_count' => (int)$this->view_count,
+            'last_reply_user_id' => (int)$this->last_reply_user_id,
+            'order' => (int)$this->order,
+            'excerpt' => $this->excerpt,
+            'slug' => $this->slug,
+            'created_at' => (string) $this->created_at,
+            'updated_at' => (string) $this->updated_at,
+            'user' => new UserResource($this->whenLoaded('user')),
+            'category' => new CategoryResource($this->whenLoaded('category')),
+        ];
     }
 }
