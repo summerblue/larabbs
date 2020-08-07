c312d70ba9330ef9e3b9dcd4238b103e9450f0a1
liyu <liyu001989@gmail.com>
2020-08-07 22:38:35 +0800

    用户权限列表

--- routes/api.php
+++ routes/api.php
@@ -101,6 +101,9 @@ Route::prefix('v1')
                     // 标记消息通知为已读
                     Route::patch('user/read/notifications', 'NotificationsController@read')
                         ->name('user.notifications.read');
+                    // 当前登录用户权限
+                    Route::get('user/permissions', 'PermissionsController@index')
+                        ->name('user.permissions.index');
                 });
 			});
     });
