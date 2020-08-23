a109cd7e11a24418fac1c1d00051c0789314d6d3
liyu <liyu001989@gmail.com>
2020-08-23 18:02:17 +0800

    weapp update user

--- routes/api.php
+++ routes/api.php
@@ -92,6 +92,8 @@ Route::prefix('v1')
                     // 编辑登录用户信息
                     Route::patch('user', 'UsersController@update')
                         ->name('user.update');
+                    Route::put('user', 'UsersController@update')
+                        ->name('user.update');
                     // 上传图片
                     Route::post('images', 'ImagesController@store')
                         ->name('images.store');
