550ef3dec8c52a5f4ea9ff554d2ec795e4dbfa30
vagrant <vagrant@homestead>
2020-11-02 09:22:46 +0000

    L3 6.5 话题列表

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -6,6 +6,9 @@ use App\Http\Requests\Api\TopicRequest;
 use App\Models\Topic;
 use Illuminate\Http\Request;
 use App\Http\Resources\TopicResource;
+use Spatie\QueryBuilder\QueryBuilder;
+use Spatie\QueryBuilder\AllowedFilter;
+use App\Models\User;
 
 class TopicsController extends Controller
 {
@@ -35,5 +38,34 @@ class TopicsController extends Controller
         $this->authorize('destroy',$topic);
         $topic->delete();
         return response(null,204);
+    }
+
+    /*
+     * 话题列表
+     */
+    public function index(){
+        $topics = QueryBuilder::for(Topic::class)
+            ->allowedIncludes('user','category')
+            ->allowedFilters([
+                'title',
+                AllowedFilter::exact('category_id'),
+                AllowedFilter::scope('withOrder')->default('recentReplied')
+            ])->paginate();
+        return TopicResource::collection($topics);
+    }
+
+    /*
+     * 某个用户的话题列表
+     */
+    public function userIndex(User $user){
+        $query = $user->topics()->getQuery();
+        $topics = QueryBuilder::for($query)
+            ->allowedIncludes('user','category')
+            ->allowedFilters([
+                'title',
+                AllowedFilter::exact('category_id'),
+                AllowedFilter::scope('withOrder')->default('recentReplied')
+            ])->where('user_id',$user->id)->paginate();
+        return TopicResource::collection($topics);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -41,6 +41,8 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function () {
         Route::get('categories','CategoriesController@show')->name('categories.show');
         // 话题列表，详情
         Route::resource('topics', 'TopicsController')->only(['index','show']);
+        // 某个用户的话题列表
+        Route::get('users/{user}/topics', 'TopicsController@userIndex')->name('users.topics.index');
         // 登录后可以访问的接口
         Route::middleware('auth:api')->group(function() {
             // 上传图片
