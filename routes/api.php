ebac25d2b105866af29945cbf802c8051cd7afbb
anyuan <676538015@qq.com>
2020-11-09 08:53:53 +0000

    API 基础配置

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::prefix('v1')->name("api.v1.")->group(function(){
+    Route::get('version',function(){
+        return "this is version v1";
+    })->name('version');
 });
