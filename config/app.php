9f9ca0550a8275a60eae0facff051aac332ba750
anyuan <676538015@qq.com>
2020-11-11 09:10:49 +0000

    短信调试

--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        App\Providers\EasySmsServiceProvider::class,
+
     ],
 
     /*
