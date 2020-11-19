5fe848aeb48875fb39285d74e85be154065c21ea
anyuan <676538015@qq.com>
2020-11-19 03:19:53 +0000

    活跃用户

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -59,4 +59,10 @@ class UsersController extends Controller
 
         return (new UserResource($user))->showSensitiveFields();
     }
+
+    public function activedIndex(User $user)
+    {
+        UserResource::wrap('data');
+        return UserResource::collection($user->getActiveUsers());
+    }
 }
