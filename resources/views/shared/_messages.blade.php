e8b86d3bb6d4560ad42085f4b3f32fe97ff4902c
Summer <summer@learnku.com>
2020-12-17 08:17:44 +0000

    基础页面结构

---
+++ resources/views/shared/_messages.blade.php
@@ -0,0 +1,9 @@
+@foreach (['danger', 'warning', 'success', 'info'] as $msg)
+  @if(session()->has($msg))
+    <div class="flash-message">
+      <p class="alert alert-{{ $msg }}">
+        {{ session()->get($msg) }}
+      </p>
+    </div>
+  @endif
+@endforeach
