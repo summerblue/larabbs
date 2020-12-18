2ae7834840f5d1b5a5a9084d4e367c7762e2fbf0
Summer <summer@learnku.com>
2020-12-18 02:32:30 +0000

    显示个人资料

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,6 +16,7 @@ class User extends Authenticatable implements MustVerifyEmailContract
         'name',
         'email',
         'password',
+        'introduction',
     ];
 
     protected $hidden = [
