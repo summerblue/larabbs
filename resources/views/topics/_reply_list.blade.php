580badc48c386898bc85e0093967dc8fe5d8e5dc
Summer <summer@learnku.com>
2020-12-18 23:16:05 +0000

    删除评论

--- resources/views/topics/_reply_list.blade.php
+++ resources/views/topics/_reply_list.blade.php
@@ -16,11 +16,19 @@
           <span class="meta text-secondary" title="{{ $reply->created_at }}">{{ $reply->created_at->diffForHumans() }}</span>
 
           {{-- 回复删除按钮 --}}
-          <span class="meta float-right ">
-            <a title="删除回复">
-              <i class="far fa-trash-alt"></i>
-            </a>
-          </span>
+          @can('destroy', $reply)
+            <span class="meta float-right">
+              <form action="{{ route('replies.destroy', $reply->id) }}"
+                  onsubmit="return confirm('确定要删除此评论？');"
+                  method="post">
+                {{ csrf_field() }}
+                {{ method_field('DELETE') }}
+                <button type="submit" class="btn btn-default btn-xs pull-left text-secondary">
+                  <i class="far fa-trash-alt"></i>
+                </button>
+              </form>
+            </span>
+          @endcan
         </div>
         <div class="reply-content text-secondary">
           {!! $reply->content !!}
