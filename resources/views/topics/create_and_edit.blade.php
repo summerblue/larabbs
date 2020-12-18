66916409b9c2e0aebff62831ef533b42a98117b5
Summer <summer@learnku.com>
2020-12-18 11:40:58 +0000

    用户可以编辑话题

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -35,10 +35,12 @@
 
               <div class="form-group">
                 <select class="form-control" name="category_id" required>
-                  <option value="" hidden disabled selected>请选择分类</option>
-                  @foreach ($categories as $value)
-                  <option value="{{ $value->id }}">{{ $value->name }}</option>
-                  @endforeach
+                  <option value="" hidden disabled {{ $topic->id ? '' : 'selected' }}>请选择分类</option>
+                    @foreach ($categories as $value)
+                      <option value="{{ $value->id }}" {{ $topic->category_id == $value->id ? 'selected' : '' }}>
+                        {{ $value->name }}
+                      </option>
+                    @endforeach
                 </select>
               </div>
 
