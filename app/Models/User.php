723ea59c33590b67c875a1c64bbe4b7a23e060a2
Summer <summer@learnku.com>
2020-12-18 11:42:26 +0000

    用户可以删除自己发的话题

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,4 +33,9 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         return $this->hasMany(Topic::class);
     }
+
+    public function isAuthorOf($model)
+    {
+        return $this->id == $model->user_id;
+    }
 }
--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -9,11 +9,11 @@ class TopicPolicy extends Policy
 {
     public function update(User $user, Topic $topic)
     {
-        return $topic->user_id == $user->id;
+        return $user->isAuthorOf($topic);
     }
 
     public function destroy(User $user, Topic $topic)
     {
-        return true;
+        return $user->isAuthorOf($topic);
     }
 }
