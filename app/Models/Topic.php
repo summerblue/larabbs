1c406d8c46afc1ccd2c9bacd69a8269ac78adfbe
Summer <summer@learnku.com>
2020-12-18 08:29:19 +0000

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -8,5 +8,18 @@ class Topic extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+    protected $fillable = [
+        'title', 'body', 'user_id', 'category_id', 'reply_count',
+        'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
