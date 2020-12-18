d94584d7ee01dc4f21afb236e8524a01bae5e6cb
Summer <summer@learnku.com>
2020-12-18 12:14:39 +0000

    回复列表

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -22,6 +22,11 @@ class Topic extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+
     public function scopeWithOrder($query, $order)
     {
         // 不同的排序，使用不同的数据读取逻辑
