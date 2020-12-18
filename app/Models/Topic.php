3898c8379583c676095f834d217123e674d569f8
Summer <summer@learnku.com>
2020-12-18 09:38:11 +0000

    用户可以创建话题

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -9,8 +9,7 @@ class Topic extends Model
     use HasFactory;
 
     protected $fillable = [
-        'title', 'body', 'user_id', 'category_id', 'reply_count',
-        'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug',
+        'title', 'body', 'category_id', 'excerpt', 'slug'
     ];
 
     public function category()
--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -1,5 +1,7 @@
 <div class="card ">
   <div class="card-body">
-    右边导航栏
+    <a href="{{ route('topics.create') }}" class="btn btn-success btn-block" aria-label="Left Align">
+      <i class="fas fa-pencil-alt mr-2"></i>  新建帖子
+    </a>
   </div>
 </div>
