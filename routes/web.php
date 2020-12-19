0d3b47fcbd83791e08ae3db7238abdd42906c0ff
Summer <summer@learnku.com>
2020-12-19 01:44:43 +0000

    首页话题列表

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 
 // 用户身份验证相关的路由
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
