ce7bde95ceea19a8579dd7f7b95e2d140d1529b9
Summer <summer@learnku.com>
2020-12-19 01:18:22 +0000

    管理后台权限

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,4 +10,14 @@ class PagesController extends Controller
     {
         return view('pages.root');
     }
+
+    public function permissionDenied()
+    {
+        // 如果当前用户有权限访问后台，直接跳转访问
+        if (config('administrator.permission')()) {
+            return redirect(url(config('administrator.uri')), 302);
+        }
+        // 否则使用视图
+        return view('pages.permission_denied');
+    }
 }
