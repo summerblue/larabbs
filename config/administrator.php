9a29b5c1b227653b2736e7bf627cc540cd98e56b
Summer <summer@learnku.com>
2020-12-19 01:14:47 +0000

    管理员可以管理权限和角色

--- config/administrator.php
+++ config/administrator.php
@@ -39,9 +39,10 @@ return array(
     'menu' => [
         '用户与权限' => [
             'users',
+            'roles',
+            'permissions',
         ],
     ],
-
     /*
      * 权限控制的回调函数。
      *
