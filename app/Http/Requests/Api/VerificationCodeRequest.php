80eb5b5f66988b6d8fd72c37199dd7ea3240aa51
liyu <liyu001989@gmail.com>
2020-12-27 22:45:43 +0800

    发送短信验证码

---
+++ app/Http/Requests/Api/VerificationCodeRequest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+class VerificationCodeRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'phone' => 'required|phone:CN,mobile|unique:users',
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('v1')->name('api.v1.')->group(function() {
+Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function () {
+    // 短信验证码
+    Route::post('verificationCodes', 'VerificationCodesController@store')
+        ->name('verificationCodes.store');
 });
