2d1a245f9863de0d1065e1fb11ac51c103fc1f31
liyu <liyu001989@gmail.com>
2020-12-27 23:16:24 +0800

    图片验证码

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -6,15 +6,25 @@ use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Overtrue\EasySms\EasySms;
 use App\Http\Requests\Api\VerificationCodeRequest;
+use Illuminate\Auth\AuthenticationException;
 
 class VerificationCodesController extends Controller
 {
     public function store(VerificationCodeRequest $request, EasySms $easySms)
     {
-        $phone = $request->phone;
+        $captchaData = \Cache::get($request->captcha_key);
 
-        // 生成4位随机数，左侧补0
-        $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+        if (!$captchaData) {
+            abort(403, '图片验证码已失效');
+        }
+
+        if (!hash_equals($captchaData['code'], $request->captcha_code)) {
+			// 验证错误就清除缓存
+            \Cache::forget($request->captcha_key);
+            throw new AuthenticationException('验证码错误');
+        }
+
+        $phone = $captchaData['phone'];
 
         if (!app()->environment('production')) {
             $code = '1234';
@@ -37,8 +47,10 @@ class VerificationCodesController extends Controller
 
         $key = 'verificationCode_'.Str::random(15);
         $expiredAt = now()->addMinutes(5);
-        // 缓存验证码 5 分钟过期。
+        // 缓存验证码 5分钟过期。
         \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+		// 清除图片验证码缓存
+		\Cache::forget($request->captcha_key);
 
         return response()->json([
             'key' => $key,
--- app/Http/Requests/Api/VerificationCodeRequest.php
+++ app/Http/Requests/Api/VerificationCodeRequest.php
@@ -4,15 +4,19 @@ namespace App\Http\Requests\Api;
 
 class VerificationCodeRequest extends FormRequest
 {
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
-            'phone' => 'required|phone:CN,mobile|unique:users',
+            'captcha_key' => 'required|string',
+            'captcha_code' => 'required|string',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'captcha_key' => '图片验证码 key',
+            'captcha_code' => '图片验证码',
         ];
     }
 }
