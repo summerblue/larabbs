faa39c535f7dbb2777063e7cba51e9d6ec1aaa81
liyu <liyu001989@gmail.com>
2021-01-02 23:13:02 +0800

    微信登录

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,6 +42,8 @@ class User extends Authenticatable implements MustVerifyEmailContract
         'password',
         'introduction',
         'avatar',
+        'weixin_openid',
+        'weixin_unionid'
     ];
 
     protected $hidden = [
