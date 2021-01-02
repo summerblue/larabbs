78ef4c2c0e1c75daa149605aa3e6a4ebea11d205
liyu <liyu001989@gmail.com>
2021-01-02 23:53:54 +0800

    用户信息

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,6 +50,8 @@ class User extends Authenticatable implements MustVerifyEmailContract, JWTSubjec
     protected $hidden = [
         'password',
         'remember_token',
+        'weixin_openid',
+        'weixin_unionid'
     ];
 
     protected $casts = [
