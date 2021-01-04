57741b02c1faa3708f6503370348118a4f739e2e
liyu <liyu001989@gmail.com>
2021-01-05 01:37:24 +0800

    话题回复

--- routes/api.php
+++ routes/api.php
@@ -75,6 +75,9 @@ Route::prefix('v1')
                     Route::resource('topics', 'TopicsController')->only([
                         'store', 'update', 'destroy'
                     ]);
+                    // 发布回复
+                    Route::post('topics/{topic}/replies', 'RepliesController@store')
+                        ->name('topics.replies.store');
                 });
             });
     });
