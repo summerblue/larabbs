1ee4050dd2cccd008700d809dd4ac04ce40de110
liyu <liyu001989@gmail.com>
2021-01-07 00:02:58 +0800

    使用 Passport

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -2,111 +2,30 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Models\User;
-use Illuminate\Support\Arr;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Auth\AuthenticationException;
 use Psr\Http\Message\ServerRequestInterface;
-use Nyholm\Psr7\Response as Psr7Response;
-use League\OAuth2\Server\AuthorizationServer;
-use App\Http\Requests\Api\AuthorizationRequest;
-use App\Http\Requests\Api\SocialAuthorizationRequest;
-use League\OAuth2\Server\Exception\OAuthServerException;
+use Laravel\Passport\Http\Controllers\AccessTokenController;
 
-class AuthorizationsController extends Controller
+class AuthorizationsController extends AccessTokenController
 {
-     public function store(AuthorizationRequest $originRequest, AuthorizationServer $server, ServerRequestInterface $serverRequest)
+    public function store(ServerRequestInterface $request)
     {
-        try {
-            $psrResponse = $server->respondToAccessTokenRequest($serverRequest, new Psr7Response);
-
-	                return (new Response(
-                $psrResponse->getBody(),
-                $psrResponse->getStatusCode(),
-                $psrResponse->getHeaders()
-            ))->setStatusCode(201);
-
-
-        } catch(OAuthServerException $e) {
-            throw new AuthenticationException($e->getMessage());
-        }
+        return $this->issueToken($request)->setStatusCode(201);
     }
 
-    public function socialStore($type, SocialAuthorizationRequest $request)
+    public function update(ServerRequestInterface $serverRequest)
     {
-        $driver = \Socialite::create($type);
-
-        try {
-            if ($code = $request->code) {
-                $oauthUser = $driver->userFromCode($code);
-            } else {
-                $tokenData['access_token'] = $request->access_token;
-
-                // 微信需要增加 openid
-                if ($type == 'wechat') {
-                    $driver->withOpenid($request->openid);
-                }
-
-                $oauthUser = $driver->userFromToken($request->access_token);
-            }
-        } catch (\Exception $e) {
-           throw new AuthenticationException('参数错误，未获取用户信息');
-        }
-
-        if (!$oauthUser->getId()) {
-           throw new AuthenticationException('参数错误，未获取用户信息');
-        }
-
-        switch ($type) {
-            case 'wechat':
-                $unionid = $oauthUser->getRaw()['unionid'] ?? null;
-
-                if ($unionid) {
-                    $user = User::where('weixin_unionid', $unionid)->first();
-                } else {
-                    $user = User::where('weixin_openid', $oauthUser->getId())->first();
-                }
-
-                // 没有用户，默认创建一个用户
-                if (!$user) {
-                    $user = User::create([
-                        'name' => $oauthUser->getNickname(),
-                        'avatar' => $oauthUser->getAvatar(),
-                        'weixin_openid' => $oauthUser->getId(),
-                        'weixin_unionid' => $unionid,
-                    ]);
-                }
-
-                break;
-        }
-
-        return $this->respondWithToken($token)->setStatusCode(201);
+        return $this->issueToken($request);
     }
 
-    public function update()
-    {
-        $token = auth('api')->refresh();
-        return $this->respondWithToken($token);
-    }
-
-        public function destroy()
+    public function destroy()
     {
         if (auth('api')->check()) {
             auth('api')->user()->token()->revoke();
             return response(null, 204);
-		} else {
+        } else {
             throw new AuthenticationException('The token is invalid.');
-		}
-	}
-
-
-    protected function respondWithToken($token)
-    {
-        return response()->json([
-            'access_token' => $token,
-            'token_type' => 'Bearer',
-            'expires_in' => auth('api')->factory()->getTTL() * 60
-        ]);
+        }
     }
 }
