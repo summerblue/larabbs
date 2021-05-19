d0f5301ed2f47178552afe7f6b0aa8938d3f153a
Summer <summer@learnku.com>
2021-05-19 08:07:41 +0800

    commented api route closure

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
