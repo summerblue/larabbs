7cfd1afdc52026df0846447eb461951ed9aa5136
Summer <summer@learnku.com>
2022-03-05 19:36:35 +0800

    修复跳转链接

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/';
 
     protected  $namespace =  'App\\Http\\Controllers'; // 添加这一行
 
