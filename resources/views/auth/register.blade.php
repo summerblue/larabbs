6a837e541fa4390223efcd8fb1b809c465548e3f
Summer <summer@learnku.com>
2022-03-05 21:39:47 +0800

    注册验证码

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,22 @@
                             </div>
                         </div>
 
+                        <div class="mb-3 row">
+                          <label for="captcha" class="col-md-4 col-form-label text-md-end">验证码</label>
+
+                          <div class="col-md-6">
+                            <input id="captcha" class="form-control{{ $errors->has('captcha') ? ' is-invalid' : '' }}" name="captcha" required>
+
+                            <img class="thumbnail captcha mt-3 mb-2" src="{{ captcha_src('flat') }}" onclick="this.src='/captcha/flat?'+Math.random()" title="点击图片重新获取验证码">
+
+                            @if ($errors->has('captcha'))
+                              <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('captcha') }}</strong>
+                              </span>
+                            @endif
+                          </div>
+                        </div>
+
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
