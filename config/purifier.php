31cb5e3c1fff6f0df7339f633a5aa5427f08bdeb
Summer <summer@learnku.com>
2022-03-06 19:17:28 +0800

    修复 XSS 注入漏洞

---
+++ config/purifier.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'encoding'         => 'UTF-8',
+    'finalize'         => true,
+    'ignoreNonStrings' => false,
+    'cachePath'        => storage_path('app/purifier'),
+    'cacheFileMode'    => 0755,
+    'settings'         => [
+        'user_topic_body' => [
+            'HTML.Doctype'             => 'XHTML 1.0 Transitional',
+            'HTML.Allowed'             => 'div,b,strong,i,em,a[href|title],ul,ol,ol[start],li,p[style],br,span[style],img[width|height|alt|src],*[style|class],pre,hr,code,h2,h3,h4,h5,h6,blockquote,del,table,thead,tbody,tr,th,td',
+            'CSS.AllowedProperties'    => 'font,font-size,font-weight,font-style,margin,width,height,font-family,text-decoration,padding-left,color,background-color,text-align',
+            'AutoFormat.AutoParagraph' => true,
+            'AutoFormat.RemoveEmpty'   => true,
+        ],
+    ],
+];
