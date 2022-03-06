1a6184aaab5c24e3d3e44fd3bb0f3fe08caec3a8
Summer <summer@learnku.com>
2022-03-06 09:25:30 +0800

    认证后的消息提醒

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -18,6 +18,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        \Illuminate\Auth\Events\Verified::class => [
+            \App\Listeners\EmailVerified::class,
+        ],
     ];
 
     /**
