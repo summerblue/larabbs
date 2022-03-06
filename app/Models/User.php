9c8b73e5ba261ba7a2cf26f50c8a788df64556cb
Summer <summer@learnku.com>
2022-03-06 10:49:07 +0800

    上传头像

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'email',
         'password',
         'introduction',
+        'avatar',
     ];
 
     protected $hidden = [
