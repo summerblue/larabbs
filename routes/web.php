fe09831ea53b6f8507a04126d9e623d8666590f9
Summer <summer@learnku.com>
2022-03-06 09:38:49 +0800

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,5 @@ Route::post('password/confirm', 'Auth\ConfirmPasswordController@confirm');
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
