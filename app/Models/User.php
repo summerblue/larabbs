10e3009da03d6934a8ba357afc892d5e2881ebf7
Summer <summer@learnku.com>
2022-03-06 09:58:34 +0800

    显示个人资料

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,32 +13,18 @@ class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable, MustVerifyEmailTrait;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'introduction',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -11,13 +11,13 @@
         <img class="card-img-top"
           src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/600/h/600"
           alt="{{ $user->name }}">
-        <div class="card-body">
-          <h5><strong>个人简介</strong></h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-          <hr>
-          <h5><strong>注册于</strong></h5>
-          <p>January 01 1901</p>
-        </div>
+          <div class="card-body">
+            <h5><strong>个人简介</strong></h5>
+            <p>{{ $user->introduction }}</p>
+            <hr>
+            <h5><strong>注册于</strong></h5>
+            <p>{{ $user->created_at->diffForHumans() }}</p>
+          </div>
       </div>
     </div>
     <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
