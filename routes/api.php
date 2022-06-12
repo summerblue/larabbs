3cf010d8bf0b1d8f41f1ad2845ccc59c052834c7
Yu Li <liyu001989@gmail.com>
2022-06-12 12:57:52 +0800

    频率限制

--- routes/api.php
+++ routes/api.php
@@ -16,12 +16,23 @@ use App\Http\Controllers\Api\VerificationCodesController;
 |
 */
 
-Route::prefix('v1')->name('api.v1.')->group(function () {
-    // 短信验证码
-    Route::post('verificationCodes', [VerificationCodesController::class, 'store'])
-        ->name('verificationCodes.store');
+Route::prefix('v1')
+    ->name('api.v1.')
+    ->group(function () {
 
-    // 用户注册
-    Route::post('users', [UsersController::class, 'store'])
-        ->name('users.store');
-});
+        Route::middleware('throttle:' . config('api.rate_limits.sign'))
+            ->group(function () {
+                // 短信验证码
+                Route::post('verificationCodes', [VerificationCodesController::class, 'store'])
+                    ->name('verificationCodes.store');
+
+                // 用户注册
+                Route::post('users', [UsersController::class, 'store'])
+                    ->name('users.store');
+            });
+
+        Route::middleware('throttle:' . config('api.rate_limits.access'))
+            ->group(function () {
+
+            });
+    });
