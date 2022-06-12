4595ea9e077779375a7d0c31c1b08e0c0b5f8b55
Yu Li <liyu001989@gmail.com>
2022-06-12 13:02:56 +0800

    propaganistas/laravel-phone

--- app/Http/Requests/Api/VerificationCodeRequest.php
+++ app/Http/Requests/Api/VerificationCodeRequest.php
@@ -7,11 +7,7 @@ class VerificationCodeRequest extends FormRequest
     public function rules()
     {
         return [
-            'phone' => [
-                'required',
-                'regex:/^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199)\d{8}$/',
-                'unique:users'
-            ]
+            'phone' => 'required|phone:CN,mobile|unique:users',
         ];
     }
 }
