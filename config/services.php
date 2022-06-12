b3140e9ebdd72e1a4cc38697a79fc75245e5899c
Yu Li <liyu001989@gmail.com>
2022-06-12 14:54:03 +0800

    微信登录

--- config/services.php
+++ config/services.php
@@ -35,4 +35,11 @@ return [
         'key'   => env('BAIDU_TRANSLATE_KEY'),
     ],
 
+    'socialite' => [
+        'wechat' => [
+            'client_id' => env('WEIXIN_KEY'),
+            'client_secret' => env('WEIXIN_SECRET'),
+            'redirect' => env('WEIXIN_REDIRECT_URI'),
+        ],
+    ]
 ];
