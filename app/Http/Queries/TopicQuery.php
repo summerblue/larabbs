ea240bbf5a40db25822f2ac24f3e48c4d40f0684
Yu Li <liyu001989@gmail.com>
2022-06-12 17:02:27 +0800

    话题详情

---
+++ app/Http/Queries/TopicQuery.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Queries;
+
+use App\Models\Topic;
+use Spatie\QueryBuilder\QueryBuilder;
+use Spatie\QueryBuilder\AllowedFilter;
+
+class TopicQuery extends QueryBuilder
+{
+    public function __construct()
+    {
+        parent::__construct(Topic::query());
+
+        $this->allowedIncludes('user', 'category')
+            ->allowedFilters([
+                'title',
+                AllowedFilter::exact('category_id'),
+                AllowedFilter::scope('withOrder')->default('recentReplied'),
+            ]);
+    }
+}
